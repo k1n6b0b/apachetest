2744500f28d59e81b49978ba2de6cda02f0c96ff
k1n6b0b <chris@cjhx.net>
2020-08-30 14:59:04 -0400

    added error handling moved cache control to php headers

--- index.php
+++ index.php
@@ -1,18 +1,81 @@
+<?php
+
+// ----------------------------------------------------------------------------------------------------
+// - Display Errors
+// ----------------------------------------------------------------------------------------------------
+ini_set('display_errors', 'On');
+ini_set('html_errors', 0);
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Reporting
+// ----------------------------------------------------------------------------------------------------
+error_reporting(-1);
+
+// ----------------------------------------------------------------------------------------------------
+// - Shutdown Handler
+// ----------------------------------------------------------------------------------------------------
+function ShutdownHandler()
+{
+    if(@is_array($error = @error_get_last()))
+    {
+        return(@call_user_func_array('ErrorHandler', $error));
+    };
+
+    return(TRUE);
+};
+
+register_shutdown_function('ShutdownHandler');
+
+// ----------------------------------------------------------------------------------------------------
+// - Error Handler
+// ----------------------------------------------------------------------------------------------------
+function ErrorHandler($type, $message, $file, $line)
+{
+    $_ERRORS = Array(
+        0x0001 => 'E_ERROR',
+        0x0002 => 'E_WARNING',
+        0x0004 => 'E_PARSE',
+        0x0008 => 'E_NOTICE',
+        0x0010 => 'E_CORE_ERROR',
+        0x0020 => 'E_CORE_WARNING',
+        0x0040 => 'E_COMPILE_ERROR',
+        0x0080 => 'E_COMPILE_WARNING',
+        0x0100 => 'E_USER_ERROR',
+        0x0200 => 'E_USER_WARNING',
+        0x0400 => 'E_USER_NOTICE',
+        0x0800 => 'E_STRICT',
+        0x1000 => 'E_RECOVERABLE_ERROR',
+        0x2000 => 'E_DEPRECATED',
+        0x4000 => 'E_USER_DEPRECATED'
+    );
+
+    if(!@is_string($name = @array_search($type, @array_flip($_ERRORS))))
+    {
+        $name = 'E_UNKNOWN';
+    };
+
+    return(print(@sprintf("%s Error in file \xBB%s\xAB at line %d: %s\n", $name, @basename($file), $line, $message)));
+};
+
+$old_error_handler = set_error_handler("ErrorHandler");
+
+// other php code
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <html>
 <head>
-	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
-	<meta http-equiv="Pragma" content="no-cache">
-	<meta http-equiv="Expires" content="0">
 	<meta http-equiv="Refresh" content="10">
 </head>
 <body>
 <h1> WOOOT <h1>
 <h2> this is my first automation! </h2>
-<h3> REV: </h3>
-<hr>
 <?php 
-// Show all information, defaults to INFO_ALL
-phpinfo();
+	echo "<h3>Last modified: " . date ("F d Y H:i:s.", filemtime($filename)) ."</h3><br><hr>"; 
+	// Show all information, defaults to INFO_ALL
+	phpinfo();
 ?>
 </body>
 </html>
